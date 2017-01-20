521169d72c91150dfecd1a9e9e9a269bcefb7c14
Preston <ludiculous89@gmail.com>
2017-01-20 19:51:23 +0700

    second

--- js/actions.js
+++ js/actions.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
   SmoothScroll();
   HandleLightBox();
   HandleLightBoxClose();
+  HandleSubmit();
+  HandleHamburgerMenu();
+  HandleResize();
+
+  HandleScroll();
 })
 
 var actionsData = new FormData();
-
+var emailForm = new FormData();
 function SmoothScroll(){
    
     $('a[href*="#"]:not([href="#"])').click(function() {
@@ -24,40 +29,207 @@ function SmoothScroll(){
 
 }
 
-function HandleLightBox(){
+var bannerClicked = false;
 
-      $('.port-wrapper').on("click",function(e){
-            
-          //$('#port-modal').css("display","block");
+function HandleLightBox(){
+  $('.port-wrapper').on("click",function(e){
+         if($('.port-modal-wrapper')){
+                $('.port-modal-wrapper').remove()
+          }
+     
+        if(!bannerClicked){
+           $('#port-modal').css({"display":"block","opacity":"1"});
 
            var bannerName = e.delegateTarget.attributes.data.value; 
             console.log(bannerName);
 
-
-            actionsData.append("bannerName",bannerName);
+             actionsData.append("bannerName",bannerName);
             
+      
+           $('html').css("overflow","hidden");
+
+            $.ajax({
+                          type: "POST",
+                          url: "../php/actions.php",
+                          data: actionsData,
+                          processData: false,
+                          cache: false,
+                          contentType: false,
+                          success: function(r) {
+                           $("#port-modal").append(r);
+                          }
+                })
+        }
+
+        bannerClicked = true;
 
 
-            if($('.port-modal-wrapper')){
-                $('.port-modal-wrapper').remove()
-            }
-           // $('html').css("overflow","hidden");
-
-        $.ajax({
-                      type: "POST",
-                      url: "../php/actions.php",
-                      data: actionsData,
-                      processData: false,
-                      cache: false,
-                      contentType: false,
-                      success: function(r) {
-                       $("#port-modal").append(r);
-                      }
-            })
       })
 }
 
+
 function HandleLightBoxClose(){
-  $('#port-modal').css("display","none");
-  $('html').css("overflow","visible");
+  $('#port-modal').on("click",function(){
+      bannerClicked = false;
+      $('#port-modal').css("display","none");
+      $('html').css("overflow","visible");
+
+  });
+  
 }
+
+function HandleHamburgerMenu(){
+    
+      $('#hamburger-menu').click(function(){
+         $('#mobile-header').css("display","block");
+      })
+      $("#mobile-header-table").click(function(){
+        $('#mobile-header').css("display","none");
+      })
+}
+
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+};
+
+
+var submitted =false;
+var formValidated = false;
+
+function HandleSubmit(){
+ 
+
+  $("#my-submit").click(function(e){
+  e.preventDefault();
+
+  var nameData = $("#name-field").val();
+  var emailData = $("#email-field").val();
+  var commentsData = $("#comments-field").val();
+  
+  var emailFormData = {
+    nameData:nameData,
+    emailData:emailData,
+    commentsData:commentsData
+  }
+
+    HandleInputUpdate();
+
+    if(!submitted){
+      HandleFormValidate(emailFormData);
+    }
+    if(formValidated){
+        emailForm.append("email",emailData);
+        emailForm.append("name",nameData);
+        emailForm.append("comments",nameData);
+       
+        $.ajax({
+                              type: "POST",
+                              url: "../php/email.php",
+                              data: emailForm,
+                              processData: false,
+                              cache: false,
+                              contentType: false,
+                              success: function(r) {
+                               console.log('sent');
+                              }
+        });
+    }
+
+    setTimeout(function(){
+        submitted = false;
+    }, 500);
+
+    submitted =true;
+  });
+}
+
+function HandleFormValidate(eFormData){
+  var validateForm = {
+    n:false,
+    e:false,
+    c:false
+  }
+   if(!isValidEmailAddress(eFormData.emailData) ) {
+     console.log(eFormData.emailData);
+      $('#email-error').css("display","block");
+    }
+    else{
+       validateForm.e = true;
+
+    }
+    if (eFormData.nameData.length < 1) {
+         $('#name-error').css("display","block");
+      }
+      else{
+         validateForm.n = true;
+      }
+  if(eFormData.commentsData.length < 1){
+      $('#comments-error').css("display","block");
+    }
+    else{
+      validateForm.c = true;
+    }
+
+     HandleFormState();
+
+    function HandleFormState(){
+       var counter = 0;
+       for(prop in validateForm){
+          if(validateForm[prop]){
+              counter++;
+          }
+        }
+        if(counter == 3){
+          formValidated = true;
+        }
+   }
+}
+
+function HandleInputUpdate(){
+   $("#name-field").on("change",function(){
+      if($(this).val().length<1){
+          $('#name-error').css("display","block");
+      }
+      else{
+        $('#name-error').css("display","none");
+      }
+   });
+
+   $("#email-field").on("change",function(){
+     if(!isValidEmailAddress($(this).val())){
+        $('#email-error').css("display","block");
+     }
+     else{
+        $('#email-error').css("display","none");
+     }
+   });
+
+   $("#comments-field").on("change",function(){
+      if($(this).val().length<1){
+        $('#comments-error').css("display","block");
+      }
+      else{
+        $('#comments-error').css("display","none");
+      }
+   });
+}
+
+function HandleResize(){
+  $(window).resize(function(){
+    if($(window).width()>968){
+      $("#mobile-header").css("display","none");
+    }
+  })
+}
+
+function HandleScroll(){
+  $(window).scroll(function(){
+     if($(window).width()<968){
+     $("#mobile-header").css("display","none");
+      }
+  })
+}  
+
+
--- php/actions.php
+++ php/actions.php
@@ -1,41 +1,67 @@
-<?php
+	<?php
 
-$bannerName = $_POST['bannerName'];
-$PhotoGroup = '';
-$TextGroup = '';
+	$bannerName = $_POST['bannerName'];
+	$PhotoGroup = '';
+	$TextGroup = '';
 
-if(isset($bannerName)){
-	if($bannerName=='vf'){
-		$PhotoGroup = '<div class="port-modal-images-inner">';
-		$PhotoGroup .= '<img src="../assets/vf-port1.jpg" />';
-		$PhotoGroup .= '<img src="../assets/vf-port2.jpg" />';
-		$PhotoGroup .= '<img src="../assets/vf-port3.jpg" />';
+	if(isset($bannerName)){
+		if($bannerName=='vf'){
+			$PhotoGroup = '<div class="port-modal-images-inner">';
+			$PhotoGroup .= '<img src="../assets/vf-port1.jpg" />';
+			$PhotoGroup .= '<img src="../assets/vf-port2.jpg" />';
+			$PhotoGroup .= '<img src="../assets/vf-port3.jpg" />';
 
-		$PhotoGroup .= '</div>';
+			$PhotoGroup .= '</div>';
 
-		$TextGroup = '<div class="port-modal-text-inner">';
-		$TextGroup .= '<div class="port-modal-text-header">ViewFind</div>';
-		$TextGroup .= '<div class="port-modal-text-body"><p>To work with ViewFind is an extreme pleasure. They are a very socially conscious group of people who </p><p>Most of my time was spent on maintaning, and improving on the existing website. Which would include bug fixes, and the creation of multi-media stories.</p></div>';
-		$TextGroup .= '<div class="port-modal-text-link"><a href="http://www.viewfind.com">Checkout ViewFind</a></div>';
-		$TextGroup .= '</div>';
+			$TextGroup = '<div class="port-modal-text-inner">';
+			$TextGroup .= '<div class="port-modal-text-header">ViewFind</div>';
+			$TextGroup .= '<div class="port-modal-text-body"><p>To work with ViewFind is an extreme pleasure. They are a socially conscious group who are creating large waves in both the photojournalism, and corporate advertisement arenas. </p>
+				<p>Most of my time was spent on maintaning, and improving on the existing website. Which would include bug fixes, and the creation of multi-media stories. The other half of my time was dedicated towards leading efforts in the creation of a new generation website. My most notable contribution is the storycreator, which is a webapp that allows users to upload images for our editorial staff to turn into visually stunning stories.</p></div>';
+			$TextGroup .= '<div class="port-modal-text-link"><a href="http://www.viewfind.com">Checkout ViewFind</a></div>';
+			$TextGroup .= '</div>';
 
+
+		}
+		elseif ($bannerName=='dk') {
+			$PhotoGroup = '<div class="port-modal-images-inner">';
+			$PhotoGroup .= '<img src="../assets/dk-port1.jpg" />';
+			$PhotoGroup .= '<img src="../assets/dk-port2.jpg" />';
+		
+			$PhotoGroup .= '</div>';
+
+			$TextGroup = '<div class="port-modal-text-inner">';
+			$TextGroup .= '<div class="port-modal-text-header">Dolls Kill</div>';
+			$TextGroup .= '<div class="port-modal-text-body"><p>DollsKill is a midsized ecommerce business, who does an amazing job at marketing and branding. Their flavor appeals to young females with a rebelous flair.</p>
+				<p>When I worked with DK my main tasks were to update landing pages, for their ever changing campaigns. The DK site ran on Magento, so I had to pick up that cms rather quickly. My biggest contribution to the company was the animated header, and an improvement on their customer service email flow.</p></div>';
+			$TextGroup .= '<div class="port-modal-text-link"><a href="http://www.dollskill.com/">Checkout Dolls Kill</a></div>';
+			$TextGroup .= '</div>';
+
+		}
+		elseif ($bannerName=='pab') {
+			$PhotoGroup = '<div class="port-modal-images-inner">';
+			$PhotoGroup .= '<img src="../assets/pab-port1.jpg" />';
+			$PhotoGroup .= '<img src="../assets/pab-port2.jpg" />';
+		
+			$PhotoGroup .= '</div>';
+
+			$TextGroup = '<div class="port-modal-text-inner">';
+			$TextGroup .= '<div class="port-modal-text-header">Precision Auto Body Shop</div>';
+			$TextGroup .= '<div class="port-modal-text-body"><p>
+			Family owned business are one of my personal favorites. Precision Auto Body is an honest shop founded by three brothers. It is pleasure to see local businesses doing great work. 
+			</p>
+				<p>My task was straightforward, create a brochure site with Wordpress. I also integrated Visualcomposer, which would allow the client to make future changes w/o coding.</p></div>';
+			$TextGroup .= '<div class="port-modal-text-link"><a href="http://precisionautobodyshop.com/">Checkout Precision Auto Body Shop</a></div>';
+			$TextGroup .= '</div>';
+		}
 
 	}
-	elseif ($bannerName=='dk') {
-		$PhotoGroup = 'DollsKill Stuff';
-	}
-	elseif ($bannerName=='pab') {
-		$PhotoGroup = 'Precision Auto Body Stuff';
-	}
 
-}
-
-$modal = '<div class="port-modal-wrapper"> ';
-$modal.= '<div class="port-modal-images">'.$PhotoGroup.'</div>';
-$modal.= '<div class="port-modal-detail">'.$TextGroup.'</div>';
-$modal.= '</div>';
+	$modal = '<div class="port-modal-wrapper"> ';
+	$modal.= '<div class="port-modal-images">'.$PhotoGroup.'</div>';
+	$modal.= '<div class="port-modal-detail">'.$TextGroup.'</div>';
+	$modal.= '</div>';
 
 
-echo $modal;
+	echo $modal;
 
-?>
+	?>
